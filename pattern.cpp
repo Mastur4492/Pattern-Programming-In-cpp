43ccb1b9efe2fbeb23391f0c3cc1da7b611e276a
Mastur <bikanerwalamastur13@gmail.com>
2025-01-21 12:12:57 +0530

    new add

--- pattern.cpp
+++ pattern.cpp
@@ -474,3 +474,25 @@
 //     cout << endl;
 //   }
 // }
+
+#include<iostream>
+using namespace std;
+
+    int main(){
+    int n = 4;
+   char ch = 'A';
+ 
+    for(int i=0; i<n; i++){ //outer loop
+    
+        for(int j=0; j<i; j++){ //first inner loop for spaces
+            cout<<" ";
+        }
+        
+        for(int j=0; j<n-i; j++){ // loop for num
+            cout<<char('A'+i);
+        }
+        cout<<endl;
+    }
+
+    return 0;
+    }
