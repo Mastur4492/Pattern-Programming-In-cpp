54b00724182e030c3b923610e865c7811951fbc6
Mastur <bikanerwalamastur13@gmail.com>
2025-01-27 14:52:07 +0530

    day 9 add

---
+++ DAY_9.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << " ";
+        }
+
+        for (int j = 0; j < (2 * (n - i) - 1); j++) {
+            if (j == 0 || j == (2 * (n - i) - 2) || i == 0) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
